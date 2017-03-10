e9f00e921c568cb9ff6658be0b83513ef7107b4e
Jonas Rodon <jonasrodon@gmail.com>
2017-03-10 16:04:36 +0100

    Node Start10

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express')
+var path = require('path')
 var app = express()
 
 app.set('port', (process.env.PORT || 5000))
 
-app.use(express.static(__dirname + '/public/'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // views is directory for all template files
 // app.set('views', __dirname + '/views')
