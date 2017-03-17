08500c0b61a4668ced6c8fdbd005045cddeb27f9
Jonas Rodon <jonasrodon@gmail.com>
2017-03-17 13:04:51 +0100

    Second Week-fix04

--- index.js
+++ index.js
@@ -13,8 +13,8 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', function (request, response) {
   response.redirect('/index.html')
 })
-app.get('/tpcastellera.cat', function (request, response) {
-  response.redirect('/tpcastellera.cat')
+app.get('/tpcastellera', function (request, response) {
+  response.redirect('/tpcastellera/index.html')
 })
 
 // app.get('/cool', function (request, response) {
