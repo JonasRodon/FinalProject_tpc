143774eea6e1ec6eaf6f905cd5d10fd967c4b628
Jonas Rodon <jonasrodon@gmail.com>
2017-03-18 15:06:48 +0100

    Adding Qr feature

--- public/js/app.js
+++ public/js/app.js
@@ -16,17 +16,18 @@
      capture: false,
      passive: false
    } : false)
-   $(document).ready(function () {
-     $('.slider8').bxSlider({
-       mode: 'vertical',
-       slideWidth: 300,
-       minSlides: 1,
-       slideMargin: 5,
-       auto: true,
-       autoControls: true,
-       speed: 2000
-     })
-   })
+   // $(document).ready(function() {
+   //     $('.slider8').bxSlider({
+   //         mode: 'vertical',
+   //         slideWidth: 300,
+   //         minSlides: 1,
+   //         slideMargin: 5,
+   //         auto: true,
+   //         autoControls: true,
+   //         speed: 2000
+   //     })
+   // })
+
    function isPassive () {
      var supportsPassiveOption = false
      try {
@@ -38,3 +39,36 @@
      } catch (e) {}
      return supportsPassiveOption
    }
+
+   $(document).ready(function () {
+     var qrcode = new QRCode(document.getElementById('qrcode'), {
+       width: 250,
+       height: 250,
+       useSVG: true
+     })
+
+     function makeCode () {
+       // var elText = document.getElementById("text");
+       var elText = document.URL
+           // if (!elText.value) {
+           //     alert("Input a text");
+           //     elText.focus();
+           //     return;
+           // }
+
+       // qrcode.makeCode(elText.value);
+       qrcode.makeCode(elText)
+     }
+
+     makeCode()
+
+   // $("#text").
+   //     on("blur", function () {
+   //         makeCode();
+   //     }).
+   //     on("keydown", function (e) {
+   //         if (e.keyCode == 13) {
+   //             makeCode();
+   //         }
+   //     });
+   })
